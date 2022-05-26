bef7c59b63cb8159643b0b9392cf2ab462275771
zohaib166 <zohaib166@gmail.com>
2022-05-26 08:31:18 +0530

    exp2

--- Recursion/recursive_power_op.cpp
+++ Recursion/recursive_power_op.cpp
@@ -3,10 +3,10 @@ using namespace std;
 
 int raise2power(int base, int exponent) {
     if(exponent==0) return 1;
-    if(exponent%2==1) 
+    if(exponent%2==1)  //if exponent is odd
         return raise2power(base, exponent/2)*raise2power(base, exponent/2)*base;
-      
-    return raise2power(base, exponent/2)*raise2power(base, exponent/2);
+    //if exponent is even  
+    return raise2power(base, exponent/2)*raise2power(base, exponent/2); 
 }   
 
 int main()
